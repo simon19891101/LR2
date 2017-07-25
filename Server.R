6d03fe75fa1023baa27b4a9dc191abdb390e4dfa
Hang <simon19891101@gmail,com>
2017-07-26 09:41:14 +1200

    changeServerR

--- Server.R
+++ Server.R
@@ -13,106 +13,7 @@ library(shinycssloaders)
 
 options(shiny.port = 7776)
 options(shiny.host = "192.168.178.29")
-#https://shiny.rstudio.com/tutorial/lesson5/
-#The server.R script is run once, when you launch your app
-#The unnamed function inside shinyServer is run once each time a user visits your app
-#The R expressions inside render* functions are run many times. Shiny runs them once each time a user changes a widget.
 
-
-
-# shinyServer(function(input,output){
-#     output$text1 = renderText(input$slider2)#take input element with id of "slider2", output in element id "text1"
-# }
-# )
-
-# shinyServer(function(input,output){
-#   output$plot1 <- renderPlot({
-#     set.seed(2016-05-25)
-#     number_of_points <- input$numeric
-#     minX <- input$sliderX[1]
-#     maxX <- input$sliderX[2]
-#     minY <- input$sliderY[1]
-#     maxY <- input$sliderY[2]
-#     dataX <- runif(number_of_points,minX,maxX)
-#     dataY <- runif(number_of_points,minY,maxY)
-#     xlab <- ifelse(input$show_xlab,"X Axis","")#checkbox returns T/F
-#     ylab <- ifelse(input$show_ylab,"Y Axis","")
-#     main <- ifelse(input$show_title,"Title","")
-#     plot(dataX,dataY,xlab=xlab,ylab=ylab,main=main,xlim=c(-100,100),ylim=c(-100,100))
-#   })
-# })
-
-# shinyServer(function(input,output){
-#   mtcars$mpgsp <- ifelse(mtcars$mpg-20>0,mtcars$mpg-20,0)
-#   model1 <- lm(hp~mpg,mtcars)
-#   model2 <- lm(hp~mpgsp+mpg,mtcars)
-#   #reactive function. Only executes when called. No unnecessary calculation.
-#   model1pred <- reactive({
-#     mpgInput <- input$sliderMPG
-#     predict(model1,newdata=data.frame(mpg=mpgInput))
-# 
-#   })
-#   #reactive function. Only executes when called. No unnecessary calculation.
-#   model2pred <- reactive({
-#     mpgInput <- input$sliderMPG
-#     predict(model2,newdata=data.frame(mpg=mpgInput,mpgsp=ifelse(mpgInput-20>20,mpgInput-20,0)))
-# 
-#   })
-# 
-#   output$plot1 <- renderPlot({
-#     mpgInput <- input$sliderMPG
-#     plot(mtcars$mpg,mtcars$hp,xlab="MPG",ylab="HP",bty="n",pch=16,xlim=c(10,35),ylim=c(50,350))
-#     if(input$showModel1){
-#       abline(model1,col="red",lwd=2)
-#     }
-#     if(input$showModel2){
-#       model2lines <- predict(model2,newdata=data.frame(mpg=10:35,mpgsp=ifelse(10:35-20>0,10:35-20,0)))
-#       lines(10:35,model2lines,col="blue",lwd=2)
-#     }
-#     legend(25,250,c("Model 1 Prediction","Model 2 Prediction"),pch=16,cex=2,col = c("red","blue"))
-#     points(mpgInput,model1pred(),col="red",cex=2,pch=16)
-#     points(mpgInput,model2pred(),col="blue",cex=2,pch=16)
-#   })
-# 
-#   output$pred1 <- renderText({
-#     model1pred()
-#   })
-# 
-#   output$Rsquare1 <- renderText({
-#     summary(model1)$r.squared
-#   })
-# 
-#   output$pred2 <- renderText({
-#     #model2pred()
-#     mpgInput <- input$sliderMPG
-#     predict(model2,newdata=data.frame(mpg=mpgInput,mpgsp=ifelse(mpgInput-20>20,mpgInput-20,0)))
-# 
-#   })
-# 
-#   output$Rsquare2 <- renderText({
-#     summary(model2)$r.squared
-#   })
-# 
-# })
-
-# shinyServer(function(input,output){
-#   library(randomForest)
-#   library(caret)
-#   data(iris)
-#   model <- train(Species~Sepal.Length,data=iris,method="rf")
-#   
-#   output$Pred <- renderText({
-#     as.character(predict(model,newdata=data.frame(Sepal.Length=input$Sepallength)))
-#   })
-#   
-# })
-
-# shinyServer(function(input,output){
-#   output$out1 <- renderPlot(hist(rnorm(input$numeric)))
-#   output$out2 <- renderText(input$box2)
-#   output$out3 <- renderText(input$box3)
-# 
-# })
 
 shinyServer(function(input,output){
   
@@ -124,30 +25,7 @@ shinyServer(function(input,output){
     lm(hp~disp,data=brushed_data)
   })
 
-  # output$slopeOut <- renderText({
-  #   if(is.null(model())){
-  #     "NA"
-  #   }else{
-  #     model()[[1]][2]#gradient/slope
-  #   }
-  # })
-  #
-  # output$intOut <- renderText({
-  #   if(is.null(model())){
-  #     "NA"
-  #   }else{
-  #     model()[[1]][1]#intercept
-  #   }
-  # })
-  #
-  # output$R2Out <- renderText({
-  #   if(is.null(model())){
-  #     "NA"
-  #   }else{
-  #     summary(model())$r.squared#rsquared
-  #   }
-  # })
-
+ 
   output$table1 <- renderTable({
     if(is.null(model())){
       table <- data.frame(Slope="NA",Intercept="NA",RSquared="NA")
@@ -204,70 +82,4 @@ shinyServer(function(input,output){
   })
 
 })
-
-######use shiny gadget to render app in R studio#######
-#Shiny apps are designed to be used by end users from web
-#Shiny Gadgets are intended to be used by R users in R studio(interactive graphics)
-# myFirstGadget <- function(){
-#   ui <- miniPage(
-#     gadgetTitleBar("My first Gadget")
-#   )
-#   server <- function(input,output,session){
-#     observeEvent(input$done,{
-#       stopApp()
-#     })
-#   }
-#   runGadget(ui,server)
-#   
-# }
-#myFirstGadget()
-
-
-# multiplyNumbers <- function(number1,number2){
-#   ui <- miniPage(
-#     gadgetTitleBar("Multiply two Numbers"),
-#     miniContentPanel(
-#       selectInput("num1","First Number",choices = number1),#sliderInput
-#       selectInput("num2","Second Number",choices = number2)
-#     )
-#   )
-#   
-#   server <- function(input,output,session){
-#     observeEvent(input$done,{
-#       num1 <- as.numeric(input$num1)
-#       num2 <- as.numeric(input$num2)
-#       stopApp(num1*num2)
-#     })
-#   }
-#   runGadget(ui,server)
-# }
-# 
-# multiplyNumbers(1:10,1:10)
-
-# pickTrees <- function(){
-#   ui <- miniPage(
-#     gadgetTitleBar("Select points by dragging your Mouse"),
-#     miniContentPanel(
-#       plotOutput("plot",height="100%",brush="brush")
-#     )
-#   )
-#   
-#   server <- function(input,output,session){
-#     output$plot <- renderPlot({#use renderVis to render GoogleVis
-#       #coltree <- 
-#       plot(trees$Girth,trees$Volume,main="Trees!",xlab="Girth",ylab="Volume")
-#       brush <- brushedPoints(trees,input$brush,xvar = "Girth",yvar="Volume")
-#       if (nrow(brush)>1){
-#         points(brush$Girth,brush$Volume,col="red")
-#       }
-#     })
-#     observeEvent(input$done,{
-#       stopApp(brushedPoints(trees,input$brush,xvar = "Girth",yvar="Volume"))
-#     })
-#   }
-#   
-#   runGadget(ui,server)
-# }
-# 
-# pickTrees()
 
